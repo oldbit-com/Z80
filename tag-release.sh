3002b1ec9f71f131b72323cceba8c82a33b6d97d
voytas <voytas@outlook.com>
2024-09-20 19:33:19 +0100

    Fix scripts

--- tag-release.sh
+++ tag-release.sh
@@ -8,8 +8,8 @@ fi
 version_regex="^v[0-9]+\.[0-9]+\.[0-9]+$"
 
 if [[ $1 =~ $version_regex ]]; then
-    git tag "$0"
-    git push origin tag "$0"
+    git tag "$1"
+    git push origin tag "$1"
 else
     echo "Invalid version format. Expected format: v[0-9]+.[0-9]+.[0-9]+"
     exit 1

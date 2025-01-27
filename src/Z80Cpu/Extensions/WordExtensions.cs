60e50a54524c9f71256105f4b7f348682d4fbd3f
voytas <voytas@outlook.com>
2025-01-27 17:09:49 +0000

    Generate docs

--- src/Z80Cpu/Extensions/WordExtensions.cs
+++ src/Z80Cpu/Extensions/WordExtensions.cs
@@ -2,8 +2,17 @@ using OldBit.Z80Cpu.Registers;
 
 namespace OldBit.Z80Cpu.Extensions;
 
+/// <summary>
+/// Extension methods for the Word type.
+/// </summary>
 public static class WordExtensions
 {
+    /// <summary>
+    /// Deconstructs the Word value into its high and low bytes.
+    /// </summary>
+    /// <param name="value">The Word value to deconstruct.</param>
+    /// <param name="highByte">The high byte of the Word value.</param>
+    /// <param name="lowByte">The low byte of the Word value.</param>
     public static void Deconstruct(this Word value, out byte highByte, out byte lowByte)
     {
         highByte = (byte)(value >> 8);
--- src/Z80Cpu/IMemory.cs
+++ src/Z80Cpu/IMemory.cs
@@ -1,7 +1,22 @@
 namespace OldBit.Z80Cpu;
 
+/// <summary>
+/// Represents a memory device that can be read from and written to.
+/// </summary>
 public interface IMemory
 {
+    /// <summary>
+    /// Reads a byte from the specified memory address.
+    /// </summary>
+    /// <param name="address">The memory address to read from.</param>
+    /// <returns>The byte read from the specified memory address.</returns>
     byte Read(Word address);
+
+
+    /// <summary>
+    /// Writes a byte to the specified memory address.
+    /// </summary>
+    /// <param name="address">The memory address to write to.</param>
+    /// <param name="data">The byte to write to the specified memory address.</param>
     void Write(Word address, byte data);
 }

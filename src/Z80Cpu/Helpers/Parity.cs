afbed9bc2a604a94bba832fec4a2dc136de06e49
voytas <voytas@outlook.com>
2025-11-04 20:27:23 +0000

    Remove unused TestBus fixture and fix Parity lookup formatting.

--- src/Z80Cpu/Helpers/Parity.cs
+++ src/Z80Cpu/Helpers/Parity.cs
@@ -12,7 +12,7 @@ internal static class Parity
     /// Contains 256 pre-calculated even/odd parity flag value for each byte value (0-255).
     /// </summary>
     internal static readonly Flags[] Lookup =
-    {
+    [
         P, 0, 0, P, 0, P, P, 0, 0, P, P, 0, P, 0, 0, P,
         0, P, P, 0, P, 0, 0, P, P, 0, 0, P, 0, P, P, 0,
         0, P, P, 0, P, 0, 0, P, P, 0, 0, P, 0, P, P, 0,
@@ -29,5 +29,5 @@ internal static class Parity
         0, P, P, 0, P, 0, 0, P, P, 0, 0, P, 0, P, P, 0,
         0, P, P, 0, P, 0, 0, P, P, 0, 0, P, 0, P, P, 0,
         P, 0, 0, P, 0, P, P, 0, 0, P, P, 0, P, 0, 0, P
-    };
+    ];
 }
